6a9f09902776c63bc3eb4064d51abac121948fa2
amiraliash <57413225+amiraliash@users.noreply.github.com>
2020-02-25 21:04:08 -0500

    All arrayLists are changed to ObservableList

--- src/daoFactories/Context.java
+++ src/daoFactories/Context.java
@@ -1,23 +1,25 @@
 package daoFactories;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import models.*;
 
 import java.util.ArrayList;
 
 public class Context {
-    public ArrayList<Food> foods;
-    public ArrayList<FoodGroup> foodGroups;
-    public ArrayList<Group> groups;
-    public ArrayList<Unit> units;
-    public ArrayList<Meal> meals;
-    public ArrayList<Location> locations;
+    public ObservableList<Food> foods;
+    public ObservableList<FoodGroup> foodGroups;
+    public ObservableList<Group> groups;
+    public ObservableList<Unit> units;
+    public ObservableList<Meal> meals;
+    public ObservableList<Location> locations;
 
     public Context() {
-        foods = new ArrayList<Food>();
-        foodGroups = new ArrayList<FoodGroup>();
-        groups = new ArrayList<Group>();
-        units = new ArrayList<Unit>();
-        meals = new ArrayList<Meal>();
-        locations = new ArrayList<Location>();
+        foods = FXCollections.observableList(new ArrayList<Food>());
+        foodGroups = FXCollections.observableList(new ArrayList<FoodGroup>());
+        groups = FXCollections.observableList(new ArrayList<Group>());
+        units = FXCollections.observableList(new ArrayList<Unit>());
+        meals = FXCollections.observableList(new ArrayList<Meal>());
+        locations = FXCollections.observableList(new ArrayList<Location>());
     }
 }
