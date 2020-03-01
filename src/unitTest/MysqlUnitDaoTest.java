c863f9e940c74dca11d04d4d01c3ec0e58b6d886
amiraliash <57413225+amiraliash@users.noreply.github.com>
2020-03-01 16:07:09 -0500

    JUnit and unit test files are added

---
+++ src/unitTest/MysqlUnitDaoTest.java
@@ -0,0 +1,32 @@
+package unitTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MysqlUnitDaoTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void all() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+}
