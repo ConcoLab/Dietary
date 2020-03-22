45e22322858c24148661d6810f8d63539c4ffd64
amiraliash <57413225+amiraliash@users.noreply.github.com>
2020-03-21 22:32:20 -0400

    connection settings are added

--- src/daoFactories/Context.java
+++ src/daoFactories/Context.java
@@ -14,22 +14,73 @@ public class Context {
     public static ObservableList<Unit> units;
     public static ObservableList<Meal> meals;
     public static ObservableList<Location> locations;
+    private Connection conn;
 
     public Context() {
         foods = FXCollections.observableList(new ArrayList<Food>());
         foodGroups = FXCollections.observableList(new ArrayList<FoodGroup>());
         groups = FXCollections.observableList(new ArrayList<Group>());
+//        units = FXCollections.observableList(new ArrayList<Unit>());
         units = FXCollections.observableList(new ArrayList<Unit>());
         meals = FXCollections.observableList(new ArrayList<Meal>());
         locations = FXCollections.observableList(new ArrayList<Location>());
-    }
 
-    public ResultSet dbCall(String sql){
         try{
             String url = "jdbc:sqlite:./src/db/dietary.db";
-            Connection conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public ResultSet getCall(String sql){
+        try{
             Statement stmt = conn.createStatement();
             ResultSet rs    = stmt.executeQuery(sql);
+            return rs;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public int deleteCall(long id, String tableName){
+        try{
+            Statement stmt = conn.createStatement();
+            int rs    = stmt.executeUpdate("DELETE FROM "+ tableName +" WHERE "+ tableName +".id = "+ id +";");
+            return rs;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    synchronized public long insertCall(String sql){
+        try{
+
+            Statement stmt = conn.createStatement();
+
+            int rs    = stmt.executeUpdate(sql);
+            if(rs == 1){
+                ResultSet generatedKeys = stmt.executeQuery("SELECT last_insert_rowid();" );
+                if (generatedKeys.next()) {
+                    System.out.println("-- DEBUG: new unit id = " + generatedKeys.getLong(1));
+
+                }
+                return generatedKeys.getLong(1);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    public ResultSet findByIdCall(long id, String tableName){
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet rs    = stmt.executeQuery("SELECT * FROM "+ tableName +" WHERE "+ tableName +".id = "+ id +" LIMIT 1;");
             return rs;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
