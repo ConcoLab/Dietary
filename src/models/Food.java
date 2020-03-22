9c2fc0f0ec49521d65b664671cde79453e6042f7
amiraliash <57413225+amiraliash@users.noreply.github.com>
2020-03-21 22:31:11 -0400

    Food model id updated

--- src/models/Food.java
+++ src/models/Food.java
@@ -5,21 +5,33 @@ The class represents abstract a Food servings that can be added to a certain Mea
 package models;
 
 
+import java.util.ArrayList;
+
 public class Food {
     private static long ITERATOR = 0;
 
     private long id;
     private String name;
     private long calories;
+    private long fat;
+    private long carbohydrate;
+    private long salt;
+    private long protein;
     private long unitId;
     private long quantity;
+    private ArrayList<Group> groups;
 
-    public Food(Long id, String name, long calories, long unitId, long quantity) {
+    public Food(long id, String name, long calories, long fat, long carbohydrate, long salt, long protein, long unitId, long quantity, ArrayList<Group> groups) {
         this.id = id;
         this.name = name;
         this.calories = calories;
+        this.fat = fat;
+        this.carbohydrate = carbohydrate;
+        this.salt = salt;
+        this.protein = protein;
         this.unitId = unitId;
         this.quantity = quantity;
+        this.groups = groups;
     }
 
     public long getId() {
@@ -62,6 +74,41 @@ public class Food {
         this.quantity = quantity;
     }
 
+    public ArrayList<Group> getGroups(){ return groups; }
 
+    public void setGroups(ArrayList<Group> groups){ this.groups = groups; }
+
+
+    public long getFat() {
+        return fat;
+    }
+
+    public void setFat(long fat) {
+        this.fat = fat;
+    }
+
+    public long getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(long carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public long getSalt() {
+        return salt;
+    }
+
+    public void setSalt(long salt) {
+        this.salt = salt;
+    }
+
+    public long getProtein() {
+        return protein;
+    }
+
+    public void setProtein(long protein) {
+        this.protein = protein;
+    }
 }
 
