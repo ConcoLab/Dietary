120078ba2c6aa71b95ddc52337c81065ebc89eed
amiraliash <57413225+amiraliash@users.noreply.github.com>
2020-04-13 10:19:58 -0400

    Tests are modified, some new changes are needed

--- src/unitTest/FoodControllerTest.java
+++ src/unitTest/FoodControllerTest.java
@@ -1,84 +1,115 @@
-package unitTest;
-
-import controllers.FoodController;
-import daoFactories.ContextFactory;
-import models.Food;
-import models.Group;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
-import views.main.MainGUI;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-/**
- * Tests functions of FoodController (and by extension, FoodDao).
- * NOTE: This class must be run in isolation or else database connection issues may cause tests to fail.
- */
-class FoodControllerTest {
-
-    static ContextFactory context;
-    static MainGUI main;
-    static ArrayList<Group> GroupList;
-
-    @BeforeAll
-     static void setUp() throws SQLException {
-        // Necessary setup for anything to function.
-        context = new ContextFactory("jdbc:sqlite:./src/db/test_FoodController.db");
-        main = new MainGUI();
-        // Setup for easily making food with groups.
-        GroupList = new ArrayList<>();
-        GroupList.add(new Group(0, "Test", new ArrayList<>()));
-    }
-
-    @AfterAll
-    static void tearDown() {
-        // TODO: Wipe the database or reset it after testing.
-        context = null;
-        main = null;
-    }
-
-    @Test
-    void getAll() throws SQLException {
-        // Get the DB size directly from the context factory. (Best access we can get.)
-        int expectedNum = ContextFactory._FoodDao().all().size();
-        // Get the same from the FoodController.
-        ArrayList<Food> foodList = FoodController.getAll();
-        int actualNum = foodList.size();
-        // Assert both sizes match.
-        assertEquals(expectedNum, actualNum);
-    }
-
-    @Test
-    void createAndGetById() throws SQLException {
-        Food newFood = new Food(0, "CreateTest", 300, 25, 40, 10, 4, 0, 2, GroupList);
-
-        // Create a new food item, getting our DB size before and after.
-        int expectedNum = ContextFactory._FoodDao().all().size() + 1;
-        long foodId = FoodController.create(newFood);
-        int actualNum = ContextFactory._FoodDao().all().size();
-        // Assert we have one more food item.
-        assertEquals(expectedNum, actualNum);
-        // Assert we can be gotten by our ID (note: can't directly compare retrieved food to our new food).
-        assertTrue(FoodController.getFoodById(foodId) != null && FoodController.getFoodById(foodId).getName().equals("CreateTest"));
-    }
-
-    @Test
-    void createAndDeleteById() throws SQLException {
-        // Create and confirm we were added.
-        Food foodToDelete = new Food(0, "DeleteMe", 300, 25, 40, 10, 4, 0, 2, GroupList);
-        int expectedNum = ContextFactory._FoodDao().all().size() + 1;
-        long foodId = FoodController.create(foodToDelete);
-        int actualNum = ContextFactory._FoodDao().all().size();
-        assertEquals(expectedNum, actualNum);
-        // Delete and confirm deletion.
-        expectedNum = ContextFactory._FoodDao().all().size() > 0 ? ContextFactory._FoodDao().all().size() - 1 : 0;
-        FoodController.delete(foodId);
-        actualNum = ContextFactory._FoodDao().all().size();
-        assertEquals(expectedNum, actualNum);
-    }
-}
+//package unitTest;
+//
+//import controllers.FoodController;
+//import daoFactories.ContextFactory;
+//import daos.concrete.FoodDao;
+//import models.Food;
+//import models.Group;
+//import models.Location;
+//import models.Unit;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import views.main.MainGUI;
+//
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+///**
+// * Tests functions of FoodController (and by extension, FoodDao).
+// * NOTE: This class must be run in isolation or else database connection issues may cause tests to fail.
+// */
+//class FoodControllerTest {
+//
+//    static ContextFactory context;
+//    static MainGUI main;
+//
+//    @BeforeAll
+//     static void setUp() throws SQLException {
+//        // Necessary setup for anything to function.
+//        context = new ContextFactory("dietaryTest");
+//        main = new MainGUI();
+//    }
+//
+//    @BeforeEach
+//    void initEach() throws SQLException {
+//        ContextFactory._FoodDao().deleteAll();
+//        ContextFactory._UnitDao().insert(new Unit(1, "Unit1"));
+//        ContextFactory._UnitDao().insert(new Unit(2, "Unit2"));
+//        ContextFactory._LocationDao().insert(new Location(1, "Location1", "Address1"));
+//        ContextFactory._LocationDao().insert(new Location(2, "Location2", "Address2"));
+//        ContextFactory._GroupDao().insert(new Group(1, "Group1", new ArrayList<Food>()));
+//        ContextFactory._GroupDao().insert(new Group(2, "Group2", new ArrayList<Food>()));
+//        ContextFactory._FoodDao().insert(new Food(1,
+//                "Food1",
+//                100,
+//                100,
+//                100,
+//                100,
+//                100,
+//                1,
+//                100,
+//                new ArrayList<Group>()));
+//        ContextFactory._FoodDao().insert(new Food(2,
+//                "Food2",
+//                100,
+//                100,
+//                100,
+//                100,
+//                100,
+//                2,
+//                100,
+//                new ArrayList<Group>(){}));
+//    }
+//
+//    @AfterAll
+//    static void tearDown() {
+//        // TODO: Wipe the database or reset it after testing.
+//        context = null;
+//        main = null;
+//    }
+//
+//    @Test
+//    void getAll() throws SQLException {
+//        // Get the DB size directly from the context factory. (Best access we can get.)
+//        int expectedNum = ContextFactory._FoodDao().all().size();
+//        // Get the same from the FoodController.
+//        ArrayList<Food> foodList = FoodController.getAll();
+//        int actualNum = foodList.size();
+//        // Assert both sizes match.
+//        assertEquals(expectedNum, actualNum);
+//    }
+//
+//    @Test
+//    void createAndGetById() throws SQLException {
+//        Food newFood = new Food(0, "CreateTest", 300, 25, 40, 10, 4, 0, 2, GroupList);
+//
+//        // Create a new food item, getting our DB size before and after.
+//        int expectedNum = ContextFactory._FoodDao().all().size() + 1;
+//        long foodId = FoodController.create(newFood);
+//        int actualNum = ContextFactory._FoodDao().all().size();
+//        // Assert we have one more food item.
+//        assertEquals(expectedNum, actualNum);
+//        // Assert we can be gotten by our ID (note: can't directly compare retrieved food to our new food).
+//        assertTrue(FoodController.getFoodById(foodId) != null && FoodController.getFoodById(foodId).getName().equals("CreateTest"));
+//    }
+//
+//    @Test
+//    void createAndDeleteById() throws SQLException {
+//        // Create and confirm we were added.
+//        Food foodToDelete = new Food(0, "DeleteMe", 300, 25, 40, 10, 4, 0, 2, GroupList);
+//        int expectedNum = ContextFactory._FoodDao().all().size() + 1;
+//        long foodId = FoodController.create(foodToDelete);
+//        int actualNum = ContextFactory._FoodDao().all().size();
+//        assertEquals(expectedNum, actualNum);
+//        // Delete and confirm deletion.
+//        expectedNum = ContextFactory._FoodDao().all().size() > 0 ? ContextFactory._FoodDao().all().size() - 1 : 0;
+//        FoodController.delete(foodId);
+//        actualNum = ContextFactory._FoodDao().all().size();
+//        assertEquals(expectedNum, actualNum);
+//    }
+//}
--- src/unitTest/GroupControllerTest.java
+++ src/unitTest/GroupControllerTest.java
@@ -2,9 +2,12 @@ package unitTest;
 
 import controllers.GroupController;
 import daoFactories.ContextFactory;
+import models.Food;
 import models.Group;
+import models.Unit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import views.main.MainGUI;
 
@@ -25,57 +28,57 @@ class GroupControllerTest {
     @BeforeAll
     static void setUp() throws SQLException {
         // Necessary setup for anything to function.
-        context = new ContextFactory("jdbc:sqlite:./src/db/test_GroupController.db");
+        context = new ContextFactory("dietaryTest");
         main = new MainGUI();
+    }
+
+    @BeforeEach
+    void initEach() throws SQLException {
+        ContextFactory._GroupDao().deleteAll();
+        ContextFactory._GroupDao().insert(new Group(1, "Group1", new ArrayList<Food>()));
+        ContextFactory._GroupDao().insert(new Group(2, "Group2", new ArrayList<Food>()));
     }
 
     @AfterAll
     static void tearDown() {
-        // TODO: Wipe the database or reset it after testing.
-        context = null;
-        main = null;
+        ContextFactory._GroupDao().deleteAll();
+//        context = null;
+//        main = null;
     }
 
     @Test
     void getAll() throws SQLException {
-        // Get from DAO (since no better option exists.)
-        int expected = ContextFactory._GroupDao().all().size();
-        // Get same value via controller and compare.
-        int actual = GroupController.getAll().size();
-        assertEquals(expected, actual);
+        ArrayList<Group> groups = GroupController.getAll();
+        assertEquals(2, groups.size());
+        assertEquals(1, groups.get(0).getId());
+        assertEquals(2, groups.get(1).getId());
+        assertEquals("Group1", groups.get(0).getName());
+        assertEquals("Group2", groups.get(1).getName());
     }
 
     @Test
     void create() throws SQLException {
         // Make a new group and check it was added.
-        int expectedNum = ContextFactory._GroupDao().all().size() + 1;
-        Group newGroup = new Group(0, "CreatedGroup", null);
-        GroupController.create(newGroup);
-        int actualNum = ContextFactory._GroupDao().all().size();
-        assertEquals(expectedNum, actualNum);
+        ContextFactory._GroupDao().insert(new Group(3, "Group3", new ArrayList<Food>()));
+        ArrayList<Group> groups = GroupController.getAll();
+
+        assertEquals(groups.size(), 3);
+        assertEquals(3, groups.get(2).getId());
+        assertEquals("Group3", groups.get(2).getName());
     }
 
-    /*@Test
-    void delete() {
-        fail("This is not testable yet since deletion depends on data accessible through the UI.");
-    }*/
+    @Test
+    void delete() throws SQLException {
+        GroupController.delete(2);
+        ArrayList<Group> groups = GroupController.getAll();
+        assertEquals(1, groups.size());
+        assertEquals(1, groups.get(0).getId());
+        assertEquals("Group1", groups.get(0).getName());
+    }
 
     @Test
-    void getGroupNames() {
-        // Ensure an empty list is an empty string.
-        String expected = "";
-        ArrayList<Group> groupList = new ArrayList<>();
-        String actual = GroupController.getGroupNames(groupList);
-        assertEquals(expected, actual);
-        // Test adding three groups.
-        expected = "FirstGroup, SecondGroup, ThirdGroup, ";
-        Group firstGroup = new Group(0, "FirstGroup", null);
-        groupList.add(firstGroup);
-        Group secondGroup = new Group(0, "SecondGroup", null);
-        groupList.add(secondGroup);
-        Group thirdGroup = new Group(0, "ThirdGroup", null);
-        groupList.add(thirdGroup);
-        actual = GroupController.getGroupNames(groupList);
-        assertEquals(expected, actual);
+    void getGroupNames() throws SQLException {
+        ArrayList<Group> groups = GroupController.getAll();
+        assertEquals("Group1, Group2, ", GroupController.getGroupNames(groups));
     }
 }
